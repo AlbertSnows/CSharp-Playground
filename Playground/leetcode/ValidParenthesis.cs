cf297963ca43bcf7e7e9553b195526a813c228c4
Albert Snow <ajsnow2012@gmail.com>
2023-10-21 16:40:18 -0500

    solve valid parenthesis

---
+++ Playground/leetcode/ValidParenthesis.cs
@@ -0,0 +1,40 @@
+﻿namespace PlaygroundTests.leetcode;
+
+public class ValidParenthesis
+{
+    public static bool 
+        validateParenthesis(string parens)
+    {
+        var parenthesisStack = new Stack<char>();
+        var openingParens = new HashSet<char>()
+        { '(', '{', '[' };
+        var closingToOpening = new Dictionary<char, char>()
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }};
+        var validSet = true;
+        foreach (var character in parens)
+        {
+            var isOpening = openingParens.Contains(character);
+            var hasOpeners = parenthesisStack.Count > 0;
+            var mostRecentParen = hasOpeners? parenthesisStack.Peek() : 'X';
+            char expectedOpener;
+            closingToOpening.TryGetValue(character, out expectedOpener);
+            var parensMatch = expectedOpener == mostRecentParen;
+            if(isOpening)
+            {
+                parenthesisStack.Push(character);
+            } else if(parensMatch)
+            {
+                parenthesisStack.Pop();
+            }
+            else
+            {
+                 validSet = false;
+                break;
+            }
+        }
+        return validSet;
+    }
+}
---
+++ PlaygroundTests/leetcode/ValidParenthesisTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundTests.leetcode;
+[TestClass]
+public class ValidParenthesisTest
+{
+    [TestMethod]
+    public void ValidateParenthesisTest()
+    {
+        var testCases = new[]{
+            (true, "()"),
+            (true, "()[]{}"),
+            (false, "(]")};
+        var expectedWithOutcome = testCases
+            .Select(pair => (
+                pair.Item1,
+                ValidParenthesis.validateParenthesis(pair.Item2)))
+            .ToList();
+        expectedWithOutcome.ForEach(pair => {
+            Assert.AreEqual(pair.Item1, pair.Item2);
+        });
+    }
+}
