2e6443ef8487f9b61b5afb4df95c1ae75fd21656
Albert Snow <ajsnow2012@gmail.com>
2023-10-21 17:56:44 -0500

    solve minimum in sorted array

--- Playground/leetcode/FindMinimumInRotatedSortedArray.cs
+++ Playground/leetcode/FindMinimumInRotatedSortedArray.cs
@@ -2,8 +2,40 @@
 
 public class FindMinimumInRotatedSortedArray
 {
-    public static int findMinimumInRotated(int[] item2)
+    public static int findMinimumInRotated(int[] nums)
     {
-        throw new NotImplementedException();
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+        while(leftIndex < rightIndex)
+        {
+            var leftValue = nums[leftIndex];
+            var rightValue = nums[rightIndex];
+            var scale = (rightIndex - leftIndex) / 2;
+
+            var middleIndex = leftIndex + scale;
+            var middleValue = nums[middleIndex];
+
+            //var leftIsBiggerThanMiddle = leftValue > middleValue;
+            var rightIsBiggerThanMiddle = rightValue > middleValue;
+            var leftIsLessThanMiddle = leftValue < middleValue;
+            //var leftIsLessThanMiddle = leftValue < middleValue;
+            //var rightIsLessThanMiddle = rightValue < middleValue;
+            var leftIsBiggerThanMiddle = leftValue > middleValue;
+            // 3 4 5 [1] 2
+            // 2 3 [4] 5 1
+            if (rightIsBiggerThanMiddle && leftIsBiggerThanMiddle) // 3 1 2
+            {
+                leftIndex = middleIndex;
+                rightIndex = middleIndex;
+            }
+            else if (rightIsBiggerThanMiddle) // 1 2 3
+            {
+                rightIndex = middleIndex;
+            } else // 2 3 1
+            {
+                leftIndex = middleIndex;
+            }
+        }
+        return nums[leftIndex];
     }
 }
