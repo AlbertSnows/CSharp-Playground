938bf4238f860890cef5e0b7aa625dd2f74c9a1c
Albert Snow <ajsnow2012@gmail.com>
2023-10-21 01:34:29 -0500

    solve 3sum

---
+++ Playground/leetcode/_3Sum.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.leetcode;
+
+public class _3Sum
+{
+
+    public static List<int[]> 
+        calc2Sum(int[] sortedNums, int target)
+    {
+        var leftIndex = 0;
+        var rightIndex = sortedNums.Length - 1;
+        var answers = new List<int[]>();
+        while(leftIndex < rightIndex) 
+        {
+            var leftValue = sortedNums[leftIndex];
+            var rightValue = sortedNums[rightIndex];
+            var currentValue = leftValue + rightValue;
+            var greaterThanTarget = currentValue > target;
+            var lessThanTarget = currentValue < target;
+            if (lessThanTarget)
+            {
+                leftIndex = leftIndex + 1;
+            } else if(greaterThanTarget)
+            {
+                rightIndex = rightIndex - 1;
+            } else
+            {
+                leftIndex = leftIndex + 1;
+                answers.Add(new[] { leftValue, rightValue });
+            }
+        }
+        return answers;
+    }
+
+    public static List<int[]> calc3Sum(int[] nums)
+    {
+        Array.Sort(nums);
+        var indexes = Enumerable.Range(0, nums.Length).ToArray();
+        var uniqueValues = new HashSet<int>();
+        var uniqueIndexes = new List<int>();
+        foreach(var index in indexes)
+        {
+            var number = nums[index];
+            var alreadyHaveIndex = uniqueValues.Contains(number);
+            if(!alreadyHaveIndex)
+            {
+                uniqueValues.Add(number);
+                uniqueIndexes.Add(index);
+            }
+        }
+        var numTo2Sums = new Dictionary<int, List<int[]>>();
+        var end = nums.Length;
+        foreach (var index in uniqueIndexes)
+        {
+            var numsFor2SumSegment = nums[(index + 1)..end];
+            var numsFor2Sum = numsFor2SumSegment
+                .ToArray();
+            var target = nums[index];
+            var _2sumsAnswers = calc2Sum(numsFor2Sum, target * -1);
+            if(_2sumsAnswers.Count > 0)
+            {
+                numTo2Sums[target] = _2sumsAnswers;
+            }
+        }
+
+        var answer = new List<int[]>();
+        foreach(var pair in numTo2Sums)
+        {
+            var target = pair.Key;
+            var _2sums = pair.Value;
+            foreach(var _2sum in _2sums) {
+                answer.Add(new[] { target, _2sum[0], _2sum[1] });
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ PlaygroundTests/leetcode/3SumTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Playground;
+using Playground.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundTests.leetcode;
+//Given an integer array nums, return all the triplets[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+//Notice that the solution set must not contain duplicate triplets.
+
+
+
+//Example 1:
+
+//Input: nums = [-1, 0, 1, 2, -1, -4]
+//Output: [[-1,-1,2], [-1,0,1]]
+//Explanation: 
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+//The distinct triplets are[-1, 0, 1] and[-1, -1, 2].
+//Notice that the order of the output and the order of the triplets does not matter.
+
+//Example 2:
+
+//Input: nums = [0, 1, 1]
+//Output: []
+//Explanation: The only possible triplet does not sum up to 0.
+
+//Example 3:
+
+//Input: nums = [0, 0, 0]
+//Output: [[0,0,0]]
+//Explanation: The only possible triplet sums up to 0.
+[TestClass]
+public class _3SumTest
+{
+    [TestMethod]
+    public void calc3Sum()
+    {
+        var testCases = new[]{
+            (new List<int[]>(){new[] { -1, -1, 2 }, new[] { -1, 0, 1 }},
+             new[] {-1,0,1,2,-1,-4}),
+            (new List<int[]>(),
+             new[] {0, 1, 1}),
+            (new List<int[]>(){ new[] {0, 0, 0 }},
+             new[] {0, 0, 0})};
+        var expectedWithOutcome = testCases
+            .Select(pair => (
+                pair.Item1,
+                _3Sum.calc3Sum(pair.Item2)))
+            .ToList();
+        expectedWithOutcome.ForEach(pair => {
+            if(pair.Item1.Count > 0)
+            {
+                var range = Enumerable.Range(0, pair.Item2.Count);
+                foreach (var index in range)
+                {
+                    // does not work
+                    var expected = pair.Item1[index];
+
+                    var outcome = pair.Item2[index];
+                    var areEqual = Enumerable.SequenceEqual(expected, outcome);
+                    Assert.IsTrue(areEqual);
+                }
+
+            }
+        });
+    }
+}
