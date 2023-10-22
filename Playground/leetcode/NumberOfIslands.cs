445561edf32565ad165252f00bfb8fc3183c1a8d
Albert Snow <ajsnow2012@gmail.com>
2023-10-21 19:24:17 -0500

    working on islands

---
+++ Playground/leetcode/NumberOfIslands.cs
@@ -0,0 +1,51 @@
+﻿namespace PlaygroundTests.leetcode;
+
+public static class NumberOfIslands
+{
+    public static int 
+        calcNumberOfIslands(string[][] grid)
+    {
+        var maxWidth = grid[0].Length;
+        var maxLength = grid.Length;
+        var visitedSquares = Enumerable.Range(0, maxLength)
+            .Select(c => Enumerable.Range(0, maxWidth)
+            .Select(r => false)
+            .ToArray())
+            .ToArray();
+
+        var rowIndex = 0;
+        var columnIndex = 0;
+        var mapIsland = buildMapIsland(maxWidth, maxLength, grid);
+        var foundIslands = 0;
+        while(columnIndex < maxLength) {
+            var value = grid[columnIndex][rowIndex];
+            var isIsland = value == "1";
+            var visited = visitedSquares[columnIndex][rowIndex];
+            var unvisitedIsland = !visited && isIsland;
+            var endOfRow = rowIndex + 1 == maxWidth;
+            if (unvisitedIsland)
+            {
+                foundIslands++;
+                visitedSquares = mapIsland(rowIndex, columnIndex, visitedSquares);
+            }
+            if(endOfRow)
+            {
+                rowIndex = 0;
+                columnIndex++;
+            } else
+            {
+                rowIndex++;
+            }
+        }
+        return foundIslands;
+    }
+
+    private static Func<int, int, bool[][], bool[][]> 
+        buildMapIsland(int maxWidth, int maxLength, string[][] grid)
+    {
+        return (rowIndex, columnIndex, visited) =>
+        {
+            return visited;
+        };
+    }
+}
---
+++ PlaygroundTests/leetcode/NumberOfIslandsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundTests.leetcode;
+[TestClass]
+internal class NumberOfIslandsTest
+{
+    [TestMethod]
+    public void ValidateParenthesisTest()
+    {
+        var testCases = new[]{
+        (1, new[]{
+            new []{"1","1","1","1","0"},
+            new []{"1","1","0","1","0"},
+            new []{"1","1","0","0","0"},
+            new []{"0","0","0","0","0"} }),
+        (3, new[]{
+            new []{"1","1","0","0","0"},
+            new []{"1","1","0","0","0"},
+            new []{"0","0","1","0","0"},
+            new []{ "0", "0", "0", "1", "1" } })};
+        var expectedWithOutcome = testCases
+            .Select(pair => (
+                pair.Item1,
+                NumberOfIslands.calcNumberOfIslands(pair.Item2)))
+            .ToList();
+        expectedWithOutcome.ForEach(pair => {
+            Assert.AreEqual(pair.Item1, pair.Item2);
+        });
+    }
+}
